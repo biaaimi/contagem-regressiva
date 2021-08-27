29617217263f1eddc790100027dc3f7e556c651c
biaaimi <biagioni.aimi@gmail.com>
2021-08-27 18:08:46 -0300

    contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace contagem_regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...");
+            Console.ReadKey();
+            Console.WriteLine("apontar...");
+            Console.ReadKey();
+            Console.WriteLine(" F O G O ");
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
